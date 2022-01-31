df871029548a7e609d867e45a25fc8649aa2dce0
JacobJax <jackjax617@mail.com>
2022-01-31 14:18:18 +0300

    initial commit

---
+++ stk_initiate.php
@@ -0,0 +1,26 @@
+<?php
+
+    $url="https://tinypesa.com/api/v1/express/initialize";
+    
+    $data = array(
+        'amount' => 1,
+        'msisdn' => '254745522419',
+        'account_no'=> 'BitRate'
+    );
+    $headers = array(
+        "Content-Type: application/json",
+        "ApiKey: XaYak3HqTpr"
+    );
+
+    $data_options = json_encode($data);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_options);
+    $resp = curl_exec($ch);
+    curl_close($ch);
+
+    var_dump($resp);
+?>
